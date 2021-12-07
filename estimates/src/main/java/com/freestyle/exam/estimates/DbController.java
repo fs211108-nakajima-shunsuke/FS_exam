a523e3769c0e7c1ae9aa060286b52662f9392d02
Nakajima <s_nakajima@freestyles.jp>
2021-12-07 23:41:40 +0900

    @1h DB接続エラーの修正

--- estimates/src/main/java/com/freestyle/exam/estimates/DbController.java
+++ estimates/src/main/java/com/freestyle/exam/estimates/DbController.java
@@ -1,34 +1,23 @@
-// package com.freestyle.exam.estimates;
+package com.freestyle.exam.estimates;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// @Controller
-// @RequestMapping("/test")
-// public class DbController {
-//     @Autowired
-//     private mst_productsRepository productRepository;
+@Controller
+@RequestMapping("/test")
+public class DbController {
+    @Autowired
+    private mst_productsRepository productRepository;
 
-//     @GetMapping
-//     public String index(Model model){
-//         List<mst_products> list = productRepository.getAll();
-//         model.addAttribute("productsList", list);
-//         return "/index";
-//     }
-// }
-
-
-// <dependency>
-// <groupId>mysql</groupId>
-// <artifactId>mysql-connector-java</artifactId>
-// <scope>runtime</scope>
-// </dependency>
-// <dependency>
-// <groupId>org.springframework.boot</groupId>
-// <artifactId>spring-boot-starter-data-jdbc</artifactId>
-// </dependency>
+    @GetMapping
+    public String index(Model model){
+        List<mst_products> list = productRepository.getAll();
+        model.addAttribute("productsList", list);
+        return "/index";
+    }
+}
--- estimates/src/main/java/com/freestyle/exam/estimates/mst_products.java
+++ estimates/src/main/java/com/freestyle/exam/estimates/mst_products.java
@@ -1,29 +1,29 @@
-// package com.freestyle.exam.estimates;
+package com.freestyle.exam.estimates;
 
-// import lombok.Data;
+import lombok.Data;
 
-// @Data
-// public class mst_products {
-//     private int cd;
-//     private String name;
-//     private int price;
+@Data
+public class mst_products {
+    private int cd;
+    private String name;
+    private int price;
 
-//     public int getCd() {
-//         return cd;
-//     }
-//     public void setCd(int cd) {
-//         this.cd = cd;
-//     }
-//     public String getName() {
-//         return name;
-//     }
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-//     public int getPrice() {
-//         return price;
-//     }
-//     public void setPrice(int price) {
-//         this.price = price;
-//     }
-// }
+    public int getCd() {
+        return cd;
+    }
+    public void setCd(int cd) {
+        this.cd = cd;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
